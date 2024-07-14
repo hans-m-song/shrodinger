39a82d471a7e140cb82b7ba0672058783c37b0cf
Hans Song <hans.m.song@gmail.com>
2024-07-15 00:36:13 +1000

    add a mutation

--- src/playbook/playbook.resolver.ts
+++ src/playbook/playbook.resolver.ts
@@ -1,6 +1,7 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Playbook } from './playbook.model';
 import { PlaybookService } from './playbook.service';
+import GraphQLJSON from 'graphql-type-json';
 
 @Resolver(() => Playbook)
 export class PlaybookResolver {
@@ -12,5 +13,22 @@ export class PlaybookResolver {
     @Args('take', { type: () => Int, nullable: true }) take: number,
   ): Promise<Playbook[]> {
     return this.playbookService.list({ skip, take });
+  }
+
+  @Mutation(() => Playbook)
+  async createPlaybook(
+    @Args({ name: 'id', type: () => String, nullable: false }) id: string,
+    @Args({ name: 'filepath', type: () => String, nullable: false })
+    filepath: string,
+    @Args({ name: 'contents', type: () => GraphQLJSON, nullable: false })
+    contents: JSON,
+  ): Promise<Playbook> {
+    return this.playbookService.create({
+      id,
+      filepath,
+      contents,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    });
   }
 }
