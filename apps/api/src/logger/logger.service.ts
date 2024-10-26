29a7405b1ffe63b98999dd5157b9dc87c1b633fd
Hans Song <hans.m.song@gmail.com>
2024-10-26 23:03:39 +1000

    logger config

--- apps/api/src/logger/logger.service.ts
+++ apps/api/src/logger/logger.service.ts
@@ -1,62 +1,65 @@
 import { Injectable, LoggerService } from '@nestjs/common';
-import init, { LogFormat } from '../config/init';
-import { PinoLogger } from 'nestjs-pino';
+import init, { LogFormat } from '@/config/init';
+import { Logger as NestjsLogger, PinoLogger } from 'nestjs-pino';
 
 @Injectable()
 export class Logger implements LoggerService {
-  instance: PinoLogger;
+  instance: NestjsLogger;
 
   static create(context = '') {
     return new Logger(context);
   }
 
   constructor(readonly context: string) {
-    this.instance = new PinoLogger({
-      renameContext: 'name',
-      pinoHttp: {
-        level: init.log.level,
-        formatters: {
-          level: (label) => ({ level: label }),
-        },
-        serializers: {
-          req: (req: any) => ({
-            id: req.id,
-            method: req.method,
-            url: req.url,
-            params: req.params,
-          }),
-          res: (res: any) => ({
-            code: res.statusCode,
-          }),
-          err: (error: any): any => {
-            if (typeof error !== 'object' || error === null) {
-              return error;
-            }
-
-            if ('toJSON' in error && typeof error.toJSON === 'function') {
-              return error.toJSON();
-            }
-
-            const { name, message, stack } = error;
-            return { name, message, stack };
+    this.instance = new NestjsLogger(
+      new PinoLogger({
+        renameContext: 'name',
+        pinoHttp: {
+          level: init.log.level,
+          formatters: {
+            level: (label) => ({ level: label }),
           },
-        },
-        transport:
-          init.log.format === LogFormat.Json
-            ? undefined
-            : {
-                target: 'pino-pretty',
-                options: {
-                  ignore: 'pid,hostname',
-                  colorize: !init.meta.ci,
+          serializers: {
+            req: (req: any) => ({
+              id: req.id,
+              method: req.method,
+              url: req.url,
+              params: req.params,
+            }),
+            res: (res: any) => ({
+              code: res.statusCode,
+            }),
+            err: (error: any): any => {
+              if (typeof error !== 'object' || error === null) {
+                return error;
+              }
+
+              if ('toJSON' in error && typeof error.toJSON === 'function') {
+                return error.toJSON();
+              }
+
+              const { name, message, stack } = error;
+              return { name, message, stack };
+            },
+          },
+          transport:
+            init.log.format === LogFormat.Json
+              ? undefined
+              : {
+                  target: 'pino-pretty',
+                  options: {
+                    ignore: 'pid,hostname',
+                    colorize: !init.meta.ci,
+                  },
                 },
-              },
-      },
-    });
+        },
+      }),
+      { renameContext: 'name' },
+    );
   }
 
   verbose(message: string | object, ...optionalParams: any[]) {
-    this.instance.trace(message, ...optionalParams);
+    this.instance.verbose(message, ...optionalParams);
   }
 
   debug(message: string | object, ...optionalParams: any[]) {
@@ -64,7 +67,7 @@ export class Logger implements LoggerService {
   }
 
   log(message: string | object, ...optionalParams: any[]) {
-    this.instance.info(message, ...optionalParams);
+    this.instance.log(message, ...optionalParams);
   }
 
   warn(message: string | object, ...optionalParams: any[]) {
